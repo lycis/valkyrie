b85d45145efa9b3615998bfba915617661a585d9
Daniel Eder <daniel@deder.at>
2015-03-30 23:51:10 +0200

    more elaborate message serialisation/deserialisation

--- src/com/github/lycis/valkyrie/communication/ValkyrieMessage.java
+++ src/com/github/lycis/valkyrie/communication/ValkyrieMessage.java
@@ -1,6 +1,8 @@
 package com.github.lycis.valkyrie.communication;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -15,13 +17,13 @@ import java.util.concurrent.atomic.AtomicLong;
 public abstract class ValkyrieMessage {
 	private static AtomicLong MSG_ID_SEQUENCE = new AtomicLong();
 	
-	private long id;
+	private ValkyrieMessageHeader header = new ValkyrieMessageHeader();
 	
 	/**
 	 * Creates an instance of the message.
 	 */
 	public ValkyrieMessage() {
-		id = MSG_ID_SEQUENCE.getAndIncrement();
+		header.setId(MSG_ID_SEQUENCE.getAndIncrement());
 	}
 	
 	/**
@@ -30,16 +32,48 @@ public abstract class ValkyrieMessage {
 	 * @return data of the message
 	 */
 	abstract public ByteArrayOutputStream getMessageData();
-
-	public int getMessageLength() {
-		return getMessageData().size();
+	
+	/**
+	 * This method is called when the message is read from the incoming channel.
+	 * When implementing your own message type this method should be used to 
+	 * deserialise the message data.
+	 * 
+	 * @param data raw message data
+	 */
+	abstract public void setMessageData(ByteArrayInputStream data);
+	
+	/**
+	 * Converts the complete message (incl. header) to a single data stream.
+	 * This is used when sending the message.
+	 *  
+	 * @return data stream of the message
+	 */
+	public ByteArrayOutputStream toDataStream() throws IOException {
+		ByteArrayOutputStream data = getMessageData();
+		header.setDataLength(data.size());
+		
+		ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+		header.toDataStream().writeTo(data);
+		data.writeTo(dataStream);
+		return data;
 	}
 	
 	/**
-	 * Provides the internal ID of this message. This ID is only valid within
-	 * one node of the peer network.
+	 * Converts a data stream into a message. This is used when reading a
+	 * message from a channel.
+	 * 
+	 * @param data raw data of the message
+	 * @throws IOException
 	 */
-	public long getId() {
-		return id;
+	public void fromDataStream(ByteArrayInputStream data) throws IOException {
+		header.fromDataStream(data);
+		setMessageData(data);
+	}
+	
+	/**
+	 * Provides the header of this message.
+	 */
+	public ValkyrieMessageHeader getHeader() {
+		return header;
 	}
 }
---
+++ src/com/github/lycis/valkyrie/communication/ValkyrieMessageHeader.java
@@ -0,0 +1,105 @@
+package com.github.lycis.valkyrie.communication;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * This is the header of a message. It identifies meta data of a message
+ * like the peer network it belongs to, the data length and the message
+ * type.
+ *  
+ * @author lycis
+ *
+ */
+public class ValkyrieMessageHeader {	
+	private long id; // node internal id of a message
+	private int messageType; // message type identifier
+	private long dataLength; // length of the data block
+	
+	public ValkyrieMessageHeader() {
+		
+	}
+	
+	public ValkyrieMessageHeader(long id) {
+		this.id = id;
+	}
+	
+	public ValkyrieMessageHeader(long id, int messageType) {
+		this(id);
+		this.messageType = messageType;
+	}
+	
+	public ValkyrieMessageHeader(long id, int messageType, long dataLength) {
+		this(id, messageType);
+		this.dataLength = dataLength;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Provides the internal ID of this message. This ID is only valid within
+	 * one node of the peer network.
+	 * @return
+	 */
+	public long getId() {
+		return id;
+	}
+	
+	public void setMessageType(int messageType) {
+		this.messageType = messageType;
+	}
+	
+	public int getMessageType() {
+		return messageType;
+	}
+	
+	public void setDataLength(long dataLength) {
+		this.dataLength = dataLength;
+	}
+	
+	public long getDataLength() {
+		return dataLength;
+	}
+	
+	/**
+	 * Seralize the message header into an output stream. This is used
+	 * when sending a message.
+	 * 
+	 * @return
+	 */
+	ByteArrayOutputStream toDataStream() throws IOException {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		stream.write(ByteBuffer.allocate(Long.BYTES).putLong(id).array());
+		stream.write(ByteBuffer.allocate(Integer.BYTES).putInt(messageType).array());
+		stream.write(ByteBuffer.allocate(Long.BYTES).putLong(dataLength).array());
+		return stream;
+	}
+	
+	/**
+	 * Fills the message header based on an input stream of raw message data.
+	 * This is used when reading a message.
+	 * 
+	 * @param data raw message data
+	 * @throws IOException
+	 */
+	void fromDataStream(ByteArrayInputStream data) throws IOException {
+		byte[] buffer;
+		
+		buffer = new byte[Long.BYTES];
+		data.read(buffer);
+		id = ByteBuffer.allocate(buffer.length).put(buffer).getLong();
+		
+		buffer = new byte[Integer.BYTES];
+		data.read(buffer);
+		messageType = ByteBuffer.allocate(buffer.length).put(buffer).getInt();
+		
+		buffer = new byte[Long.BYTES];
+		data.read(buffer);
+		dataLength = ByteBuffer.allocate(buffer.length).put(buffer).getLong();
+	}
+}
